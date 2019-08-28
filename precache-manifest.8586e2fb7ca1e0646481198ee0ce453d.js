76ec40ed68c07c5d59476674423d615914168792
Joshua McGehee <mcgehee.joshua@gmail.com>
2019-08-28 17:46:46 -0600

    Updates

---
+++ precache-manifest.8586e2fb7ca1e0646481198ee0ce453d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2638aed620d59f6c431ec6cdcfb95f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06b7dfd8383b7c10c841",
+    "url": "/static/css/main.2082b349.chunk.css"
+  },
+  {
+    "revision": "140b37af77e1030375df",
+    "url": "/static/js/2.6fbf8391.chunk.js"
+  },
+  {
+    "revision": "06b7dfd8383b7c10c841",
+    "url": "/static/js/main.a7a577ff.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  }
+]);
